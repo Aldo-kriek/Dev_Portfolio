cd6f1508139d47762ac5cf3282d72d1bf1678511
Aldo-kriek <118925007+Aldo-kriek@users.noreply.github.com>
2023-03-13 23:36:05 +0200

    Add files via upload

---
+++ inc/sendEmail.php
@@ -0,0 +1,14 @@
+﻿<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = "Email From Contact Form";
+
+$message = $name . " " . $email . " wrote the following: " . "\n\n" . $_POST['message'];
+
+mail($name, $subject, $message);
+
+echo "Mail Sent. Thank you " . $name . ", will contact you shortly.";
+
+?>
+
