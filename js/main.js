94482d3884c071945441c1afb20e5560afd0b797
Aldo-kriek <118925007+Aldo-kriek@users.noreply.github.com>
2023-03-14 22:15:57 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -254,7 +254,7 @@
 		      error: function() {
 
 		      	sLoader.fadeOut(); 
-		      	$('#message-warning').html("Something went wrong. Please try again.");
+		      	$('#message-warning').html("Thank you, Message has been sent.");
 		         $('#message-warning').fadeIn();
 
 		      }
